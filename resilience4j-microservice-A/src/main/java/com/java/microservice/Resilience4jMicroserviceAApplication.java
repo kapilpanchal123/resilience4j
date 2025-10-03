bce64f880b2bf980e8d4baa815cd81ed096c7367
admin <admin@SPL1581>
2025-10-03 15:08:24 +0530

    [init] resilience4j microservice A

---
+++ resilience4j-microservice-A/src/main/java/com/java/microservice/Resilience4jMicroserviceAApplication.java
@@ -0,0 +1,13 @@
+package com.java.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Resilience4jMicroserviceAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Resilience4jMicroserviceAApplication.class, args);
+	}
+
+}
